147a30a0c23ae3e53b90cfc181d3fa2c55ff5fb7
Lev <levdanneman@gmail.com>
2021-02-09 16:06:32 +0200

    portfolio start

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.getElementById("demo").onclick = function () {
+//   myFunction();
+// };
+
+const display = (e, link) => {
+  let tabLink = document.querySelectorAll(".tablink");
+  for (let i = 0; i < tabLink.length; i++) {
+    tabLink[i].classList.remove("highlight");
+  }
+  let text = document.querySelectorAll(".text");
+  for (let i = 0; i < text.length; i++) {
+    text[i].classList.add("display-none");
+  }
+  document.getElementById(link).classList.remove("display-none");
+
+  e.currentTarget.classList.add("highlight");
+};
+
+
