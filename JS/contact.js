82c39fbb91ab4cdffdc9ac1d407d59ea5c9be25d
Lev <levdanneman@gmail.com>
2021-02-13 20:12:12 +0200

    google form

--- JS/contact.js
+++ JS/contact.js
@@ -1,9 +1,4 @@
-const submitBtn = document.querySelector(".submit");
-
-const submitForm = (e) => {
-    e.preventDefault();
-  //   e.stopImmediatePropagation();
-  console.log("hello");
-};
-
-submitBtn.addEventListener("click", submitForm);
+document.querySelector(".submit").addEventListener("submit", function (e) {
+  e.preventDefault();
+  e.stopImmediatePropagation();
+});
--- JS/projects.js
+++ JS/projects.js
@@ -1,17 +1,24 @@
 const projectPage = document.querySelector("#project-root");
+const icon = document.querySelector("#icon");
 
 const queryString = location.search.substring(1);
 console.log(queryString);
 
 const petAdoption = () => {
+  icon.href = "../IMAGES/petsproj.png";
+  icon.type = "image/png";
   projectPage.innerHTML = "Pet Adoption";
 };
 
 const stickyNote = () => {
+  icon.href = "../IMAGES/stickynote.jpg";
+  icon.type = "image/gif";
   projectPage.innerHTML = "Sticky Note";
 };
 
 const hackathon = () => {
+  icon.href = "../IMAGES/shift.jpg";
+  icon.type = "image/gif";
   projectPage.innerHTML = "Hackathon";
 };
 
